c9a11ef3910d82736dea4320bf6b3629743b7dde
joaobassiDenver <137333862+joaobassiDenver@users.noreply.github.com>
2025-02-27 23:17:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,15 +5,15 @@ import plotly.graph_objects as go
 import numpy as np
 from io import BytesIO
 
-his_cota = pd.read_excel("Quadros para Relatório Gerencial.xlsx", sheet_name="Historico_Cota")
+his_cota = pd.read_excel("https://github.com/joaobassiDenver/ClubeKairos/blob/7383259ae891e75341d9c10c11f05ce58a68f1ab/Quadros%20para%20Relat%C3%B3rio%20Gerencial.xlsx", sheet_name="Historico_Cota")
 
-cotistas = pd.read_excel("Quadros para Relatório Gerencial.xlsx", sheet_name="Cotistas")
+cotistas = pd.read_excel("https://github.com/joaobassiDenver/ClubeKairos/blob/7383259ae891e75341d9c10c11f05ce58a68f1ab/Quadros%20para%20Relat%C3%B3rio%20Gerencial.xlsx", sheet_name="Cotistas")
 
-ativos = pd.read_excel("Quadros para Relatório Gerencial.xlsx", sheet_name="Ativos")
+ativos = pd.read_excel("https://github.com/joaobassiDenver/ClubeKairos/blob/7383259ae891e75341d9c10c11f05ce58a68f1ab/Quadros%20para%20Relat%C3%B3rio%20Gerencial.xlsx", sheet_name="Ativos")
 
-rent_mensal = pd.read_excel("Quadros para Relatório Gerencial.xlsx", sheet_name="Rent_Mensal", header=2)
+rent_mensal = pd.read_excel("https://github.com/joaobassiDenver/ClubeKairos/blob/7383259ae891e75341d9c10c11f05ce58a68f1ab/Quadros%20para%20Relat%C3%B3rio%20Gerencial.xlsx", sheet_name="Rent_Mensal", header=2)
 
-selic_hoje = pd.read_excel("Quadros para Relatório Gerencial.xlsx", sheet_name="Selic")
+selic_hoje = pd.read_excel("https://github.com/joaobassiDenver/ClubeKairos/blob/7383259ae891e75341d9c10c11f05ce58a68f1ab/Quadros%20para%20Relat%C3%B3rio%20Gerencial.xlsx", sheet_name="Selic")
 
 nome_clube = "CLUBE DE INVESTIMENTO KAIROS"
 data_atual = his_cota["Data "].max()
