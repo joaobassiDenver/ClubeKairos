6633e3d25e0b7ed1f0e9a085dac05292810e19d7
João Bassi <joao.bassi@denvercontabil.com.br>
2026-03-24 18:20:24 -0300

    Update header parameter for Rent_Mensal sheet in app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ cotistas = pd.read_excel("Relatório_Gerencial_Denver.xlsx" ,sheet_name="Cotista
 
 ativos = pd.read_excel("Relatório_Gerencial_Denver.xlsx" ,sheet_name="Ativos")
 
-rent_mensal = pd.read_excel("Relatório_Gerencial_Denver.xlsx" ,sheet_name="Rent_Mensal", header=6)
+rent_mensal = pd.read_excel("Relatório_Gerencial_Denver.xlsx" ,sheet_name="Rent_Mensal", header=9)
 
 selic_hoje = pd.read_excel("Relatório_Gerencial_Denver.xlsx" ,sheet_name="Selic")
 
